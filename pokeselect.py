294be8b0648876f67c68a585788646b086a57d30
fpxx777 <renato.fox.castillo@alumnos.sip.cl>
2024-04-18 10:06:59 -0400

    First commit

--- pokeselect.py
+++ pokeselect.py
@@ -11,8 +11,8 @@ def pokeselect():
         if len(pokemon[num]["tipo"]) == 2:
             print(f'Tipo: {pokemon[num]["tipo"][0]} y {pokemon[num]["tipo"][1]}')
         else:
-            print(f'Tipo: {pokemon[num]["tipo"][0]}')
-        print("Movimientos:")
+            print(f'TIPO: {pokemon[num]["tipo"][0]}')
+        print("MOVIMIENTOS:")
         for move in pokemon[num]["ataques"]:
                 print(f'{move},')
 pokeselect()
