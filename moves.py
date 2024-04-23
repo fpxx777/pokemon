142b37c7c8e2db1abb03ade655fd79fc41e9e224
fpxx777 <renato.fox.castillo@alumnos.sip.cl>
2024-04-23 11:42:55 -0400

    Avanzanding

---
+++ moves.py
@@ -0,0 +1,25 @@
+import json
+
+class Movimiento:
+    def __init__(self, nombre, tipo, clase, potencia, precision, prioridad, pp, efecto):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.clase = clase
+        self.potencia = potencia
+        self.precision = precision
+        self.prioridad = prioridad
+        self.pp = pp
+        self.efecto = efecto
+
+def jsoncreate():
+    with open("JSON/moves.json", "r") as file:
+        data = json.load(file)
+        return data
+
+data = jsoncreate()
+
+movimientos = [Movimiento(**movimiento) for movimiento in data]
+
+print(movimientos[0].nombre)
+
+
--- poke.py
+++ poke.py
@@ -6,9 +6,17 @@ class Stats:
         self.ataqueEspecial = stats[3]
         self.defensaEspecial = stats[4]
         self.velocidad = stats[5]
+
     def __str__(self) -> str:
         return f"HP: {self.hp}\nAtque: {self.ataque}\nDefensa: {self.defensa}\nAtaque Especial: {self.ataqueEspecial}\nDefensa Especial: {self.defensaEspecial}\nVelocidad: {self.velocidad}"
 
+
+class Ataques:
+        def __init__(self, nombre, posicion):
+                self.nombre = nombre
+                self.posicion_json = posicion
+        def __repr__(self):
+                return f"Nombre: {str(self.nombre)}, Posicion en JSON: {str(self.posicion_json)}"
 
 class Pokemon:
     def __init__(self, nombre, tipo, ataques, estadisticas):
@@ -17,29 +25,42 @@ class Pokemon:
         self.ataques = ataques
         self.stats = Stats(estadisticas)
 
-venasaur = Pokemon("Venasaur",
-                ["Planta",
-                "Veneno"],
-                ["Rayo Solar",
-                "Somnifero",
-                "Sintesis",
-                "Latigazo"],[80, 82, 83, 100, 100, 80
-        ])
 
-charizard = Pokemon("Charizard",
-                ["Fuego",
-                "Volador"],
-                ["Lanzallamas",
-                "Infierno",
-                "Garra Dragon",
-                "Envite Igneo"],[78, 84, 78, 100, 109, 85
-        ])
+venasaur = Pokemon(
+    "Venasaur",
+    ["Planta", "Veneno"],
+    [
+        Ataques("Rayo Solar", 0),
+        Ataques("Somnifero", 1),
+        Ataques("Sintesis", 2),
+        Ataques("Latigazo", 3),
+    ],
+    [80, 82, 83, 100, 100, 80],
+)
 
-blastoise = Pokemon("Blastoise",
-                ["Agua"],
-                ["Hidrobomba",
-                "Defensa Ferrea",
-                "Danza Lluvia",
-                "Envite Acuatico"],[79, 83, 100, 78, 85, 78
-        ])
+charizard = Pokemon(
+    "Charizard",
+    ["Fuego", "Volador"],
+    [
+        Ataques("Lanzallamas", 4),
+        Ataques("Infierno", 5),
+        Ataques("Garra Dragon", 6),
+        Ataques("Envite Igneo", 7),
+    ],
+    [78, 84, 78, 100, 109, 85],
+)
+
+blastoise = Pokemon(
+    "Blastoise",
+    ["Agua"],
+    [
+        Ataques("Hidrobomba", 8),
+        Ataques("Defensa Ferrea", 9),
+        Ataques("Danza Lluvia", 10),
+        Ataques("Envite Acuatico", 11),
+    ],
+    [79, 83, 100, 78, 85, 78],
+)
 pokemon = [venasaur, charizard, blastoise]
+
+print(venasaur.ataques)
